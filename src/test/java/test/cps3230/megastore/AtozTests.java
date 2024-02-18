0298bb81727f5d43baa2a5c0a06daf62d49e26da
DamFil <damjan.filipovic.21@um.edu.mt>
2024-02-18 14:11:09 +0100

    implemented tests for 4 other categories

--- src/test/java/test/cps3230/megastore/AtozTests.java
+++ src/test/java/test/cps3230/megastore/AtozTests.java
@@ -45,8 +45,9 @@ public class AtozTests {
         driver.quit();
     }
 
+    // cameras category
     @Test
-    public void testTakenToCorrectCategory() throws Exception {
+    public void testCamerasCategory() {
         // setup
         // find the electronics button and hover over it for the submenu to appear
         WebElement electronicsBtn = driver.findElement(By.id("hcategory_674"));
@@ -63,6 +64,154 @@ public class AtozTests {
         WebElement parentContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("s-grid-9")));
         WebElement categoryName = parentContainer.findElement(By.className("category-name"));
         Assertions.assertEquals(categoryName.getText(), "CAMERAS");
+
+        // verify that there are at least 3 products
+        WebElement products = parentContainer.findElement(By.className("products"));
+        List<WebElement> productList = products.findElements(By.xpath(".//div[contains(@class, 'product')]"));
+        int numProducts = productList.size();
+        Assertions.assertTrue(numProducts >= 3);
+
+        // verify that clicking on the first product in the results we get the details page of that product
+        WebElement firstProduct = productList.getFirst();
+        WebElement firstProductClickable = firstProduct.findElement(By.className("product-inner-wrap"));
+        String firstProductName = firstProduct.findElement(By.className("productname")).getText();
+        firstProductClickable.click();
+
+        WebElement nameOfClickedProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("name")));
+        String detailsPageName = nameOfClickedProduct.getText();
+        Assertions.assertEquals(firstProductName.toUpperCase(), detailsPageName.toUpperCase());
+    }
+
+    // cameras category
+    @Test
+    public void testSSDCategory() {
+        // setup
+        // find the electronics button and hover over it for the submenu to appear
+        WebElement electronicsBtn = driver.findElement(By.id("hcategory_674"));
+        Actions hoverOver = new Actions(driver);
+        hoverOver.moveToElement(electronicsBtn).perform();
+        // wait for the submenu to appear
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement ssdBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div/ul/li[@id='hcategory_163034']")));
+        // move the mouse over the cameras category and click on it
+        hoverOver.moveToElement(ssdBtn).click().perform();
+
+        // verify
+        // wait for the product container element to appear and check that the category name is "Cameras"
+        WebElement parentContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("s-grid-9")));
+        WebElement categoryName = parentContainer.findElement(By.className("category-name"));
+        Assertions.assertEquals(categoryName.getText(), "SSD");
+
+        // verify that there are at least 3 products
+        WebElement products = parentContainer.findElement(By.className("products"));
+        List<WebElement> productList = products.findElements(By.xpath(".//div[contains(@class, 'product')]"));
+        int numProducts = productList.size();
+        Assertions.assertTrue(numProducts >= 3);
+
+        // verify that clicking on the first product in the results we get the details page of that product
+        WebElement firstProduct = productList.getFirst();
+        WebElement firstProductClickable = firstProduct.findElement(By.className("product-inner-wrap"));
+        String firstProductName = firstProduct.findElement(By.className("productname")).getText();
+        firstProductClickable.click();
+
+        WebElement nameOfClickedProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("name")));
+        String detailsPageName = nameOfClickedProduct.getText();
+        Assertions.assertEquals(firstProductName.toUpperCase(), detailsPageName.toUpperCase());
+    }
+
+    // category power banks
+    @Test
+    public void testPowerBankCategory() {
+        // setup
+        // find the electronics button and hover over it for the submenu to appear
+        WebElement electronicsBtn = driver.findElement(By.id("hcategory_674"));
+        Actions hoverOver = new Actions(driver);
+        hoverOver.moveToElement(electronicsBtn).perform();
+        // wait for the submenu to appear
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement powerBanksBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div/ul/li[@id='hcategory_1460']")));
+        // move the mouse over the cameras category and click on it
+        hoverOver.moveToElement(powerBanksBtn).click().perform();
+
+        // verify
+        // wait for the product container element to appear and check that the category name is "Cameras"
+        WebElement parentContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("s-grid-9")));
+        WebElement categoryName = parentContainer.findElement(By.className("category-name"));
+        Assertions.assertEquals(categoryName.getText(), "POWER BANKS");
+
+        // verify that there are at least 3 products
+        WebElement products = parentContainer.findElement(By.className("products"));
+        List<WebElement> productList = products.findElements(By.xpath(".//div[contains(@class, 'product')]"));
+        int numProducts = productList.size();
+        Assertions.assertTrue(numProducts >= 3);
+
+        // verify that clicking on the first product in the results we get the details page of that product
+        WebElement firstProduct = productList.getFirst();
+        WebElement firstProductClickable = firstProduct.findElement(By.className("product-inner-wrap"));
+        String firstProductName = firstProduct.findElement(By.className("productname")).getText();
+        firstProductClickable.click();
+
+        WebElement nameOfClickedProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("name")));
+        String detailsPageName = nameOfClickedProduct.getText();
+        Assertions.assertEquals(firstProductName.toUpperCase(), detailsPageName.toUpperCase());
+    }
+
+    // category
+    @Test
+    public void testMichellinTyresCategory() {
+        // setup
+        // find the electronics button and hover over it for the submenu to appear
+        WebElement tyresBtn = driver.findElement(By.id("headlink88"));
+        Actions hoverOver = new Actions(driver);
+        hoverOver.moveToElement(tyresBtn).perform();
+        // wait for the submenu to appear
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement michellinBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div/ul/li[@id='hcategory_69556']")));
+        // move the mouse over the cameras category and click on it
+        hoverOver.moveToElement(michellinBtn).click().perform();
+
+        // verify
+        // wait for the product container element to appear and check that the category name is "Cameras"
+        WebElement parentContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("s-grid-9")));
+        WebElement categoryName = parentContainer.findElement(By.className("category-name"));
+        Assertions.assertEquals(categoryName.getText(), "MICHELIN");
+
+        // verify that there are at least 3 products
+        WebElement products = parentContainer.findElement(By.className("products"));
+        List<WebElement> productList = products.findElements(By.xpath(".//div[contains(@class, 'product')]"));
+        int numProducts = productList.size();
+        Assertions.assertTrue(numProducts >= 3);
+
+        // verify that clicking on the first product in the results we get the details page of that product
+        WebElement firstProduct = productList.getFirst();
+        WebElement firstProductClickable = firstProduct.findElement(By.className("product-inner-wrap"));
+        String firstProductName = firstProduct.findElement(By.className("productname")).getText();
+        firstProductClickable.click();
+
+        WebElement nameOfClickedProduct = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("name")));
+        String detailsPageName = nameOfClickedProduct.getText();
+        Assertions.assertEquals(firstProductName.toUpperCase(), detailsPageName.toUpperCase());
+    }
+
+    // category fencing
+    @Test
+    public void testFaceCareCategory() {
+        // setup
+        // find the electronics button and hover over it for the submenu to appear
+        WebElement tyresBtn = driver.findElement(By.id("hcategory_698"));
+        Actions hoverOver = new Actions(driver);
+        hoverOver.moveToElement(tyresBtn).perform();
+        // wait for the submenu to appear
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement fencingBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div/ul/li[@id='hcategory_74706']")));
+        // move the mouse over the cameras category and click on it
+        hoverOver.moveToElement(fencingBtn).click().perform();
+
+        // verify
+        // wait for the product container element to appear and check that the category name is "Cameras"
+        WebElement parentContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("s-grid-9")));
+        WebElement categoryName = parentContainer.findElement(By.className("category-name"));
+        Assertions.assertEquals(categoryName.getText(), "FENCING");
 
         // verify that there are at least 3 products
         WebElement products = parentContainer.findElement(By.className("products"));
